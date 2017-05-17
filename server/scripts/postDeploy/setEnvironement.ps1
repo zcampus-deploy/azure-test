972d681f3a7fb8eefefd2ecfc4d9ecdd5a445902
Azure Deploy <merjadok@gmail.com>
2017-05-17 14:29:31 +0200

    Changing the correct file

--- server/scripts/postDeploy/setEnvironement.ps1
+++ server/scripts/postDeploy/setEnvironement.ps1
@@ -1,5 +1,9 @@
+######################################################
+# Sets environment to 'production' for deployed site #
+# on Azure by changing the config/config.json file   #
+######################################################
 Write-Output "Setting environment to 'production'"
-$pathToJson = 'server\config\environment.json'
+$pathToJson = '..\wwwroot\config\environment.json'
 $a = Get-Content $pathToJson -raw | ConvertFrom-Json
 $a.name = "production"
 $a | ConvertTo-Json  | set-content $pathToJson
